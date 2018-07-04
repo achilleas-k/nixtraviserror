7982c2ce4a4e88513d28fca11bde95b0de1e1232
Achilleas Koutsou <achilleas.k@gmail.com>
2018-07-04 14:10:15 +0200

    Run using python

---
+++ test_stuff.py
@@ -0,0 +1,29 @@
+import os
+from subprocess import Popen, PIPE
+import tempfile
+
+from compile import maketests
+
+
+BINDIR = tempfile.mkdtemp(prefix="nixpy-tests-")
+
+maketests(BINDIR)
+
+
+def runcpp(command, *args):
+    cmdbin = os.path.join(BINDIR, command)
+    cmdargs = [cmdbin]
+    cmdargs.extend(args)
+    proc = Popen(cmdargs, stdout=PIPE, stderr=PIPE)
+    proc.wait()
+    stdout = proc.stdout.read().decode()
+    stderr = proc.stderr.read().decode()
+    # print(stdout)
+    # print(stderr)
+    if proc.returncode:
+        raise ValueError(stdout+stderr)
+
+
+def test_full_file_read():
+    nixfilepath = "filetest-readpy.nix"
+    runcpp("writefullfile", nixfilepath)
